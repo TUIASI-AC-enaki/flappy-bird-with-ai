521ffdb244f89b97df7fd4b9df4ee92fce761cbe
invo12 <alexandru.ilioi@student.tuiasi.ro>
2020-11-28 17:37:18 +0200

    Made one bird to fly alone

--- training.py
+++ training.py
@@ -20,7 +20,7 @@ def create_pipe():
 
 def move_pipes(pipes):
     for pipe in pipes:
-        pipe.centerx -= 5
+        pipe.centerx -= velocity
     return pipes
 
 
@@ -33,19 +33,19 @@ def draw_pipes(pipes):
             screen.blit(flip_pipe, pipe)
 
 
-def check_collision(pipes):
+def check_collision(pipes, bird_rect):
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
-            return False
+            return True
 
     if bird_rect.top <= -100 or bird_rect.bottom >= 900:
-        return False
+        return True
 
-    return True
+    return False
 
 
-def rotate_bird(bird):
-    new_bird = pygame.transform.rotozoom(bird, -bird_movement * 3, 1)
+def rotate_bird(bird, index):
+    new_bird = pygame.transform.rotozoom(bird, -bird_movement[index] * 3, 1)
     return new_bird
 
 
@@ -56,9 +56,11 @@ clock = pygame.time.Clock()
 
 # Game Variables
 gravity = 0.25
-bird_movement = 0
 game_active = True
 score = 0
+velocity = 5
+number_of_birds = 1
+bird_movement = [0 for _ in range(number_of_birds)]
 
 background_sf = pygame.image.load("assets/background-day.png").convert()
 background_sf = pygame.transform.scale2x(background_sf)
@@ -68,12 +70,13 @@ floor_sf = pygame.transform.scale2x(floor_sf)
 floor_x = 0
 
 bird_surface = pygame.transform.scale2x(pygame.image.load("assets/bluebird-midflap.png").convert_alpha())
-bird_rect = bird_surface.get_rect(center=(100, 512))
-FLY = pygame.USEREVENT + 1
-fly_event = pygame.event.Event(FLY)
-#pygame.time.set_timer(FLY, 850)
-bird_cromoshome = Chromosome(NeuralBird())
-print(bird_cromoshome)
+bird_rects = [bird_surface.get_rect(center=(100, 512)) for _ in range(number_of_birds)]
+active_birds = [True] * number_of_birds
+FLY = [pygame.USEREVENT + i for i in range(1, number_of_birds + 1)]
+fly_events = [pygame.event.Event(FLY[i]) for i in range(number_of_birds)]
+
+# pygame.time.set_timer(FLY, 850)
+bird_cromoshomes = [Chromosome(NeuralBird()) for _ in range(number_of_birds)]
 
 pipe_surface = pygame.image.load("assets/pipe-green.png").convert()
 pipe_surface = pygame.transform.scale2x(pipe_surface)
@@ -93,26 +96,51 @@ while True:
             if len(pipe_list) > 4:
                 del pipe_list[0]
                 del pipe_list[0]
-        if event.type == FLY:
-            bird_movement = 0
-            bird_movement -= 12
+        if event.type in FLY:
+            ind = event.type - FLY[0]
+            bird_movement[ind] = 0
+            bird_movement[ind] -= 12
     screen.blit(background_sf, (0, 0))
 
     if game_active:
         # Bird
-        bird_movement += gravity
-        rotated_bird = rotate_bird(bird_surface)
-        bird_rect.centery += bird_movement
-        screen.blit(rotated_bird, bird_rect)
+        bird_movement = list(map(lambda x: x + gravity, bird_movement))
+        rotated_bird = [rotate_bird(bird_surface, i) for i in range(number_of_birds)]
+        for index in range(number_of_birds):
+            if active_birds[index]:
+                bird_rects[index].centery += bird_movement[index]
+                screen.blit(rotated_bird[index], bird_rects[index])
+                # te uiti la coliziuni
+                if check_collision(pipe_list, bird_rects[index]):
+                    active_birds[index] = False
+                    bird_cromoshomes[index].fitness = score
 
-        # te uiti la coliziuni
-        check_collision(pipe_list)
-        write_to_json_file([bird_cromoshome.to_dict()])
+
+        if len(pipe_list) > 0:
+            print(pipe_list[1].midbottom[1])
+        distance = 10000
+        pipe_up = 10000
+        pipe_down = 10000
+
         # dai update la neuronii de input
-        # dai compute la noua valoare. Daca e True generezi event
-        #       pygame.event.post(fly_event)
+        for i in range(0, len(pipe_list), 2):
+            distance = pipe_list[i].centerx - bird_rect.centerx
+            pipe_down = pipe_list[0].midtop[1] - bird_rect.centery
+            pipe_up = pipe_list[0].midbottom[1] - bird_rect.centery
+            if distance > 0:
+                break
 
-        #cand mor toti faci gameActive = false
+        bird_cromoshome.bird.update_inputs(distance=distance, bird_height=bird_rect.centery,
+                                           pipe_bottom_height=pipe_down, pipe_top_height=pipe_up, velocity=velocity)
+
+        # dai compute la noua valoare. Daca e True generezi event
+        print(bird_cromoshome.bird.compute_output())
+        if bird_cromoshome.bird.compute_output():
+            pygame.event.post(fly_event)
+
+        # cand mor toti faci gameActive = false
+        game_active = check_collision(pipe_list, bird_rect)
+
         # Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
@@ -121,10 +149,11 @@ while True:
     else:
         # alg genetic
 
-        #salvare parametrii cel mai bun fitness
+        # salvare parametrii cel mai bun fitness
 
-        #resetare populatie
-        game_active = True
+        # resetare populatie
+        # game_active = True
+        write_to_json_file([bird_cromoshomes[i].to_dict() for i in range(number_of_birds)])
         print("game over")
     # Floor
     floor_x -= 1
